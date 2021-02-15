d3adcb7fdb19fc74ac1b4e1cacd98374e5209fb2
Hsiao Feng <vectorst@foxmail.com>
2021-02-15 16:04:42 +0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -2,7 +2,7 @@ from qqwry import QQwry
 import openpyxl
 num = 100 #查询的总行数
 file = 'ip.xlsx' #文件名，需要在同一目录下
-key = 0 #0=只显示地区，1=只显示详细地址（如某某网吧）
+key = 0 #0=只显示地区（如广东省深圳市），1=只显示详细地址（如某某网吧）
 q = QQwry()
 q.load_file('qqwry.dat')
 row = 1
