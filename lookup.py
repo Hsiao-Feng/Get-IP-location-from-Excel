5d00f4661aea613d4e68249801fa53fd0a3fd7d9
Hsiao Feng <vectorst@foxmail.com>
2021-02-15 15:50:37 +0800

    Create lookup.py

---
+++ lookup.py
@@ -0,0 +1,11 @@
+from qqwry import QQwry
+import openpyxl
+num = 100 #查询的总行数
+file = 'ip.xlsx' #文件名，需要在同一目录下
+key = 0 #0=只显示地区，1=只显示详细地址（如某某网吧）
+q = QQwry()
+q.load_file('qqwry.dat')
+row = 1
+while row<=num:
+    print(q.lookup(openpyxl.load_workbook(file)['Sheet1'].cell(row=row,column=1).value)[key])
+    row += 1
